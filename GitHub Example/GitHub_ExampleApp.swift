714cbf0a29613371e808ed9ddf9978bea6abc184
jswota <joels@waltersinc.com>
2024-08-12 13:30:01 -0400

    Initial Commit

---
+++ GitHub Example/GitHub_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExampleApp.swift
+//  GitHub Example
+//
+//  Created by Joel Swota on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
